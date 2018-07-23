73e57332e2879eea48c5449647391d7b5b52233b
skhimsara <skhimsara@gmail.com>
2018-07-23 00:47:11 -0700

    bug fix and added ipython notebook

--- Smile_or_Frown/smile_or_frown_detector.py
+++ Smile_or_Frown/smile_or_frown_detector.py
@@ -70,7 +70,8 @@ test_dir = os.path.join(base_dir, "test")
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D,AveragePooling2D,  BatchNormalization,  Activation
-
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
 
 INPUT_IMAGE_SIZE=64
 COLOR_DEPTH_DIM=1
@@ -115,20 +116,20 @@ for data_batch, labels_batch in train_generator:
 
 
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu',
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu',
                         input_shape=(INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE, COLOR_DEPTH_DIM)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Flatten())
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-4),
